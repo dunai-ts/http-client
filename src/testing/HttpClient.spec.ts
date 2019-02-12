f0d47612185609247376f7378170d004c723c75b
Lubyanoy <lubyanoy@squilla.io>
2019-02-12 20:07:12 +0300

    HttpMock

--- src/testing/HttpClient.spec.ts
+++ src/testing/HttpClient.spec.ts
@@ -1,86 +1,200 @@
 import { describe, it } from 'mocha';
-import * as should from 'should';
+import should from 'should';
 import { Response } from '../Http';
-import { Http, HttpMock } from './HttpClient';
+import { HttpClient } from '../HttpClient';
+import { HttpMock } from './HttpClient';
 
-console.log(HttpMock);
+let Http: HttpClient;
 
 describe('HTTP proxy tests', () => {
-    it('List of requests', async () => {
+    beforeEach(() => {
         HttpMock.flush();
-        const result = Http.get('https://google.com');
-
-        const req = HttpMock.expectOne('https://google.com');
-        req.answer(Response.createOk({}));
-
-        console.log(result);
-        console.log(await result);
-
-        // await Http.post('https://google.com', {q: 'asdasd'});
-        console.log(HttpMock.requests);
-        // should(HttpMock.requests).have.length(2);
     });
 
-    //     it('Mock get remote request', async () => {
-    //         HttpMock.reset();
-    //         HttpMock.setMiddleware((method, url, body) => {
-    //             return new Promise((resolve, reject) => {
-    //                 should(method).be.equal('get');
-    //                 should(url).be.equal('http://example.com');
-    //                 setTimeout(() => {
-    //                     let response = Response.createOk({
-    //                         ok: true
-    //                     });
-    //                     resolve(response);
-    //                 }, 200);
-    //             });
-    //         });
-    //         const result = await Http.get('http://example.com');
-    //         should(result.statusCode).be.equal(200);
-    //         should(result.statusMessage).be.equal('Ok');
-    //         should(result.status).be.equal('200: Ok');
-    //         should(result.body).have.property('ok')
-    //             .be.equal(true);
-    //     });
-    //
-    //     it('Mock post remote request', async () => {
-    //         HttpMock.reset();
-    //         HttpMock.setMiddleware((method, url, body) => {
-    //             return new Promise((resolve, reject) => {
-    //                 should(method).be.equal('post');
-    //                 should(url).be.equal('http://example.com');
-    //                 setTimeout(() => {
-    //                     let response = Response.createOk({
-    //                         ok  : true,
-    //                         data: body['data']
-    //                     });
-    //                     resolve(response);
-    //                 }, 200);
-    //             });
-    //         });
-    //         const result = await Http.post('http://example.com', { data: 'value' });
-    //         should(result.statusCode).be.equal(200);
-    //         should(result.statusMessage).be.equal('Ok');
-    //         should(result.status).be.equal('200: Ok');
-    //         should(result.body).have.property('ok')
-    //             .be.equal(true);
-    //         should(result.body).have.property('data')
-    //             .be.equal('value');
-    //     });
-    //
-    //     it('Remove middleware', async () => {
-    //         HttpMock.reset();
-    //         HttpMock.setMiddleware((method, url, body) => {
-    //             return new Promise((resolve, reject) => {
-    //                 throw new Error('Can not be run');
-    //             });
-    //         });
-    //         HttpMock.removeMiddleware();
-    //         let result = await Http.get('http://example.com');
-    // //        should(result.statusCode).be.equal(200);
-    // //        should(result.statusText).be.equal('Ok');
-    // //        should(result.status).be.equal('200: Ok');
-    // //        should(result.body).have.property('ok')
-    // //                           .be.equal(true);
-    //     });
+    describe('expectRequests, expect*', () => {
+        beforeEach(() => {
+            Http = new HttpClient();
+            Http.rootMiddleware.after.push(HttpMock.middleware);
+            HttpMock.flush();
+
+            Http.get('https://google.com/search?q=sdfsdf');
+            Http.post('https://google.com', {});
+            Http.put('https://ya.ru', {});
+        });
+
+        describe('url', () => {
+            it('all', () => {
+                const reqs = HttpMock.expectRequests([], /.*/);
+                should(reqs).length(3);
+            });
+
+            it('url as string 1', () => {
+                const reqs = HttpMock.expectRequests([], 'yahoo');
+                should(reqs).length(0);
+            });
+
+            it('url as string 2', () => {
+                const reqs = HttpMock.expectRequests([], 'sdfsdf');
+                should(reqs).length(1);
+            });
+
+            it('url as RegExp 2', () => {
+                const reqs = HttpMock.expectRequests([], /e\.com/);
+                should(reqs).length(2);
+            });
+
+            it('url as RegExp 1', () => {
+                const reqs = HttpMock.expectRequests([], /e\.com$/);
+                should(reqs).length(1);
+            });
+        });
+
+        describe('method', () => {
+            it('all', () => {
+                const reqs = HttpMock.expectRequests([], /.*/);
+                should(reqs).length(3);
+            });
+
+            it('get', () => {
+                const reqs = HttpMock.expectRequests(['get'], /.*/);
+                should(reqs).length(1);
+            });
+
+            it('post', () => {
+                const reqs = HttpMock.expectRequests(['post'], /.*/);
+                should(reqs).length(1);
+            });
+
+            it('put', () => {
+                const reqs = HttpMock.expectRequests(['put'], /.*/);
+                should(reqs).length(1);
+            });
+
+            it('options', () => {
+                const reqs = HttpMock.expectRequests(['options'], /.*/);
+                should(reqs).length(0);
+            });
+        });
+
+        describe('count (total 3)', () => {
+            it('1..5', () => {
+                const reqs = HttpMock.expectRequests([], /.*/, 1, 5);
+                should(reqs).length(3);
+            });
+
+            it('= 3', () => {
+                const reqs = HttpMock.expectRequests([], /.*/, 3, 3);
+                should(reqs).length(3);
+            });
+
+            it('1..2', () => {
+                should(() => HttpMock.expectRequests([], /.*/, 1, 2))
+                    .throwError('More items than it should be');
+            });
+
+            it('5..7', () => {
+                should(() => HttpMock.expectRequests([], /.*/, 5, 7))
+                    .throwError('Elements less than it should be');
+            });
+        });
+
+        describe('expect*', () => {
+            it('expectOne (== 1)', () => {
+                const req = HttpMock.expectOne([], 'ya.ru');
+                should(req).ok();
+            });
+
+            it('expectOne (more 1)', () => {
+                should(() => HttpMock.expectOne([], 'google.com'))
+                    .throw('More items than it should be');
+            });
+
+            it('expectNoRequests', () => {
+                should(() => HttpMock.expectNoRequests())
+                    .throw('More items than it should be');
+            });
+
+            it('expectNoRequests (negative)', () => {
+                HttpMock.requests.length = 0;
+
+                should(() => HttpMock.expectNoRequests())
+                    .not.throwError('Elements less than it should be');
+            });
+
+            it('expectNone', () => {
+                should(() => HttpMock.expectNone([], 'yahoo'))
+                    .not.throw();
+            });
+
+            it('expectNone (negative)', () => {
+                should(() => HttpMock.expectNone([], 'sdf'))
+                    .throw('More items than it should be');
+            });
+
+            it('expectNone at all', () => {
+                HttpMock.flush();
+                should(() => HttpMock.expectNone())
+                    .not.throw();
+            });
+
+            it('expectNone at all (negative)', () => {
+                should(() => HttpMock.expectNone())
+                    .throw('More items than it should be');
+            });
+        });
+    });
+    describe('requests and viewed status', () => {
+        it('List of requests and viewed status', async () => {
+            const q = Http.get('https://google.com');
+
+            should(HttpMock.requests).have.length(1);
+            should(HttpMock.requests[0]).have.property('viewed', false);
+
+            const req = HttpMock.expectOne('https://google.com');
+            req.answer(Response.createOk({ok: 'OK'}));
+
+            should(HttpMock.requests).have.length(1);
+            should(HttpMock.requests[0]).have.property('viewed', true);
+
+            const res = await q;
+
+            should(res.body).eql({ok: 'OK'});
+        });
+    });
+
+    describe('other', () => {
+        it('Mock get remote request', async () => {
+            const q = Http.get('http://example.com');
+
+            console.log(HttpMock.requests);
+
+            const req = HttpMock.expectOne('get', 'http://example.com');
+            req.answer(Response.createOk({
+                ok: true,
+            }));
+
+            const result = await q;
+
+            should(result.statusCode).be.equal(200);
+            should(result.statusMessage).be.equal('OK');
+            should(result.status).be.equal('200: OK');
+            should(result.body).have.property('ok', true);
+        });
+
+        it('Mock post remote request', async () => {
+            const q = Http.post('http://example.com', {data: 'value'});
+
+            const req = HttpMock.expectOne('post', 'http://example.com');
+            req.answer(Response.createOk({
+                ok  : true,
+                data: req.request.body['data'],
+            }));
+            const result = await q;
+            should(result.statusCode).be.equal(200);
+            should(result.statusMessage).be.equal('OK');
+            should(result.status).be.equal('200: OK');
+            should(result.body).have.property('ok', true);
+            should(result.body).have.property('data', 'value');
+        });
+    });
 });
--- src/testing/HttpClient.ts
+++ src/testing/HttpClient.ts
@@ -9,6 +9,7 @@ interface IRequestItem {
     request: Request;
     // next: (req: Request) => Promise<Response>;
     answer: (res: Response) => void;
+    viewed: boolean;
 }
 
 export class HttpClientMock {
@@ -18,6 +19,15 @@ export class HttpClientMock {
         this.middleware = this.middleware.bind(this);
     }
 
+    public middleware(request: Request, _: Response, makeQuery: (req: Request) => void, giveAnswer: (res: Response) => void) {
+        this.requests.push({
+            request,
+            // next: makeQuery,
+            answer: giveAnswer,
+            viewed: false,
+        });
+    }
+
     public flush() {
         this.requests = [];
     }
@@ -25,38 +35,54 @@ export class HttpClientMock {
     public expectOne(url: string | RegExp): IRequestItem;
     public expectOne(methods: string | string[], url: string | RegExp): IRequestItem;
     public expectOne(methods: string | string[] | RegExp, url?: string | RegExp): IRequestItem {
-        if (url)
-            return this._expectOne(typeof methods === 'string' ? [methods] : methods, url);
+        if (url === void 0)
+            return this.expectRequests([], methods as string | RegExp, 1, 1)[0];
         else
-            return this._expectOne([], methods as string | RegExp);
+            return this.expectRequests(typeof methods === 'string' ? [methods] : methods as string[], url, 1, 1)[0];
     }
 
-    public expectNone(url: string | RegExp): IRequestItem;
-    public expectNone(methods: string | string[], url: string | RegExp): IRequestItem;
-    public expectNone(methods: string | string[] | RegExp, url?: string | RegExp): IRequestItem {
-        if (url)
-            return this._expectNone(typeof methods === 'string' ? [methods] : methods, url);
+    public expectNone(url?: string | RegExp): void;
+    public expectNone(methods: string | string[], url: string | RegExp): void;
+    public expectNone(methods: string | string[] | RegExp, url?: string | RegExp): void {
+        if (url === void 0)
+            this.expectRequests([], methods as string | RegExp, 0, 0);
         else
-            return this._expectNone([], methods as string | RegExp);
+            this.expectRequests(typeof methods === 'string' ? [methods] : methods as string[], url, 0, 0);
     }
 
-    public middleware(request: Request, _: Response, makeQuery: (req: Request) => void, giveAnswer: (res: Response) => void) {
-        this.requests.push({
-            request,
-            // next: makeQuery,
-            answer: giveAnswer,
-        });
+    public expectNoRequests(): void {
+        if (this.requests.length)
+            throw new Error('More items than it should be');
     }
 
-    private _expectOne(methods: string[] | RegExp, url: string | RegExp): IRequestItem {
-        return this.requests[0];
-    }
+    public expectRequests(methods: string[], url: string | RegExp, min = 0, max = Infinity): IRequestItem[] {
+        const items: IRequestItem[] = this.requests
+            .filter(item => {
+                if (methods.length && methods.indexOf(item.request.method.toLowerCase()) === -1)
+                    return false;
 
-    private _expectNone(methods: string[] | RegExp, url: string | RegExp): IRequestItem {
-        return null;
+                if (typeof item.request.url === 'string')
+                    if (!item.request.url.match(url))
+                        return false;
+
+                return true;
+            });
+
+        if (items.length < min)
+            throw new Error('Elements less than it should be');
+        if (items.length > max)
+            throw new Error('More items than it should be');
+
+        items.forEach(item => item.viewed = true);
+
+        return items;
     }
 }
 
 export const Http = new HttpClient();
 export const HttpMock = new HttpClientMock();
 Http.rootMiddleware.after.push(HttpMock.middleware);
+
+// function isViewed(item: IRequestItem): boolean {
+//     return item.viewed;
+// }
